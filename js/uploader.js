fba2a158e93a3ac5a64fc5974314f1965cfe62ac
Michael Zedeler <michael@zedeler.dk>
2014-10-18 01:09:47 +0200

    Removed unused tmp filename creation.

--- js/uploader.js
+++ js/uploader.js
@@ -6,34 +6,25 @@ var tar = require('tar-fs'),
     glob = require('glob'),
     fs = require('fs'),
     querystring = require('querystring'),
-    tmp = require('tmp'),
     rm = require('shelljs').rm;
 
 module.exports = function(path, serverUrl, key, callback) {
-  tmp.tmpName(
-    function (err, tmpPath) {
-      if(err) {
-        callback('Unable to create temporary file');
-      } else {
-        var options = url.parse(serverUrl + '?' + querystring.stringify({key: key}));
-        options.method = 'POST';
-        options.headers = {
-          'Content-Type': 'application/octet-stream'
-        };
-        var req = http.request(options, function(response) {
-          var body = '';
-          response.on('data', function(chunk) { body += chunk; });
-          response.on('end', function() {
-            var error;
-            if(response.statusCode !== 200) {
-              error = body.trim();
-            }
-            callback(error);
-          });
-        });
-        var archive = tar.pack(path);
-        archive.pipe(req);
-      }
-    }
-  );
+    var options = url.parse(serverUrl + '?' + querystring.stringify({key: key}));
+    options.method = 'POST';
+    options.headers = {
+      'Content-Type': 'application/octet-stream'
+    };
+    var req = http.request(options, function(response) {
+      var body = '';
+      response.on('data', function(chunk) { body += chunk; });
+      response.on('end', function() {
+        var error;
+        if(response.statusCode !== 200) {
+          error = body.trim();
+        }
+        callback(error);
+      });
+    });
+    var archive = tar.pack(path);
+    archive.pipe(req);
 };
